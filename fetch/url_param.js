37204b08ad4b0cb2c5c962b6335c72b3d8375d39
ghaluccas <gabsgit@gmail.com>
2024-12-16 15:09:47 -0300

    token fetch

---
+++ fetch/url_param.js
@@ -0,0 +1,19 @@
+const FetchUrl = async()=>{
+    const BaseUrl = 'https://jsonplaceholder.typicode.com/posts';
+    const params =  new URLSearchParams({ userId: 1 }); // Add query params
+
+    try{
+        const response = await fetch(`${BaseUrl}?${params}`);
+        if (response.ok){
+            data = await response.json();
+            console.log(data);
+        }else {
+            throw new Error("HTTP Failed : ",response.status);
+        }
+    }catch(erro){
+        console.log("ERROR : ",erro);
+    }
+
+}
+
+FetchUrl()
